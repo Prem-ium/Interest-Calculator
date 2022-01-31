605a39a4ca335e3f25e9231f1a4fc1df412ed9ac
Prem Patel <80719066+Vrem0@users.noreply.github.com>
2022-01-30 23:10:26 -0500

    implemented daily estimate

--- src/APYInterestCalculator.java
+++ src/APYInterestCalculator.java
@@ -6,7 +6,7 @@ public class APYInterestCalculator {
 			Scanner input = new Scanner(System.in);
 		){
 			// Instantiate Variables
-			double apy, apr, balance, interest, sum = 0;
+			double apy, apr, balance, interest, accured = -1, sum = 0;
 			String choice = "";
 			float comp = 1f;
 			
@@ -14,12 +14,13 @@ public class APYInterestCalculator {
 			System.out.print("Enter your APY (Annual Percentage Yield): ");
 			apy = input.nextDouble();
 			
+			// Create error/exception if APY input is negative value
 			if (apy < 0) {
 				throw new UnsupportedInputException(apy, "APY cannot be negative");
 			}
 			
 			// Prompt user for compounding-frequency of APY
-			System.out.printf("%nDaily = 0\tWeekly = 1\tMonthly = 2\tSemi-Annually = 3%n");
+			System.out.printf("%nDaily = 0\tWeekly = 1\tMonthly = 2%n");
 			System.out.print("How often does your Annual Percentage Yield Compound?: ");
 			
 			// Fill Number of Periods value and store choice
@@ -27,29 +28,24 @@ public class APYInterestCalculator {
 			
 			case 0:
 				comp = 365f;
+				accured = 365;
 				choice = "daily";
 				break;
 			case 1:
 				comp = 52f;
+				accured = 7;
 				choice = "weekly";
 				break;
 			case 2:
 				comp = 12f;
+				accured = 31;
 				choice = "monthly";
-				break;
-			case 3:
-				comp = 6f;
-				choice = "semi-annual";
-				break;
-			case 4:
-				comp = 1f;
-				choice = "annual";
 				break;
 			}
 			// Calculate and print APR
 			apr = (((Math.pow((apy / 100) + 1, (1/comp))) - 1) * comp) * 100;
 			
-			System.out.printf("%nAn APY of %.2f%c compounding %s is equivalant to: %.2f%c APR"
+			System.out.printf("%nAn APY of %.2f%c compounding %s is equivalant to: %.2f%c APR%n"
 														, apy, '%', choice, apr, '%');
 			
 			// Trim APR to two decimal positions
@@ -57,9 +53,10 @@ public class APYInterestCalculator {
 			apr = (apr / 100) / comp;
 			
 			// Prompt user for balance
-			System.out.printf("%nPlease insert your balance to see your calculated %s payment: "
-																				, choice);
+			System.out.printf("%nPlease enter your balance to calculate your estimated %s interest: ", choice);
 			balance = input.nextDouble();
+			
+			// Throw an error if a negative value is entered
 			if (balance < 0) {
 				throw new UnsupportedInputException(balance, "balance cannot be negative");
 			}
@@ -67,16 +64,22 @@ public class APYInterestCalculator {
 			System.out.println();
 			System.out.println();
 			
-			for(int i = 0; i <= comp; i++) {
+			for(int i = 1; i <= comp; i++) {
 				interest = (balance * apr) / 100;
 				balance += interest;
 				sum += interest;
-				System.out.printf("%s INTEREST #%d:\tInterest: $%,.4f\tBalance: $%,.2f%n",
-												choice.toUpperCase(), i, interest, balance);
+				System.out.printf("%s INTEREST #%d:"
+								+ "%n\t\t\t Daily Estimated Interest: $%,.4f"
+								+ "%n\t\t\t Total Earned Interest: $%,.4f"
+								+ "%n\t\t\t Updated Balance: $%,.2f %n",
+												choice.toUpperCase(), i, interest / accured ,interest, balance);
 			}
-			
-			System.out.printf("%n%nWith a starting balance of $%,.2f: you will have accumulated a total of $%,.2f interest with a %s compounding rate.",
-															balance - sum, sum, choice);
+			System.out.printf("%n%n"
+							+ "Starting Balance:\t\t$%,.2f %n"
+							+ "Earning %s interest at: \t%.2f APY %n"
+							+ "Total Interest Earned in Year:\t$%,.2f %n"
+							+ "Ending Balance:\t\t\t$%,.2f", 
+							balance - sum, choice, apy, sum, balance);
 		} catch (UnsupportedInputException e) {
 			System.out.println(e.getMessage());
 		} catch (Exception e) {
