ef3a9367c8f4afefb298f3d4499c6faf230b6658
Prem Patel <80719066+sazncode@users.noreply.github.com>
2022-07-13 16:23:29 -0400

    Update APYInterestCalculator.java

--- src/APYInterestCalculator.java
+++ src/APYInterestCalculator.java
@@ -1,124 +1,93 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class APYInterestCalculator {
-	public static void main(String[] args) throws UnsupportedInputException {
-		try (
-			Scanner input = new Scanner(System.in);
-		){
-			// Instantiate Variables
-			double apy, apr, balance, interest, accured = -1, sum = 0;
-			String choice = "";
-			float comp = 1f;
-			
-			// Prompt user for APY
-			System.out.print("Enter your APY (Annual Percentage Yield): ");
-			apy = input.nextDouble();
-			
-			// Create error/exception if APY input is negative value
-			if (apy < 0) {
-				throw new UnsupportedInputException(apy, "APY cannot be negative");
-			}
-			
-			// Prompt user for compounding-frequency of APY
-			System.out.printf("%nDaily = 0\tWeekly = 1\tMonthly = 2%n");
-			System.out.print("How often does your Annual Percentage Yield Compound?: ");
-			
-			// Fill Number of Periods value and store choice
-			switch (input.nextInt()) {
-			
-			case 0:
-				comp = 365f;
-				accured = 365;
-				choice = "daily";
-				break;
-			case 1:
-				comp = 52f;
-				accured = 7;
-				choice = "weekly";
-				break;
-			case 2:
-				comp = 12f;
-				accured = 31;
-				choice = "monthly";
-				break;
-			}
-			// Calculate and print APR
-			apr = (((Math.pow((apy / 100) + 1, (1/comp))) - 1) * comp) * 100;
-			
-			System.out.printf("%nAn APY of %.2f%c compounding %s is equivalant to: %.2f%c APR%n"
-														, apy, '%', choice, apr, '%');
-			
-			// Trim APR to two decimal positions
-			apr = Math.round(apr * 100.0);
-			apr = (apr / 100) / comp;
-			
-			// Prompt user for balance
-			System.out.printf("%nPlease enter your balance to calculate your estimated %s interest: ", choice);
-			balance = input.nextDouble();
-			
-			// Throw an error if a negative value is entered
-			if (balance < 0) {
-				throw new UnsupportedInputException(balance, "balance cannot be negative");
-			}
-			
-			System.out.println();
-			System.out.println();
-			
-			for(int i = 1; i <= comp; i++) {
-				interest = (balance * apr) / 100;
-				balance += interest;
-				sum += interest;
-				System.out.printf("%s INTEREST #%d:"
-								+ "%n\t\t\t Daily Estimated Interest: $%,.4f"
-								+ "%n\t\t\t Total Earned Interest: $%,.4f"
-								+ "%n\t\t\t Updated Balance: $%,.2f %n",
-												choice.toUpperCase(), i, interest / accured ,interest, balance);
-			}
-			System.out.printf("%n%n"
-							+ "Starting Balance:\t\t$%,.2f %n"
-							+ "Earning %s interest at: \t%.2f APY %n"
-							+ "Total Interest Earned in Year:\t$%,.2f %n"
-							+ "Ending Balance:\t\t\t$%,.2f", 
-							balance - sum, choice, apy, sum, balance);
-		} catch (UnsupportedInputException e) {
-			System.out.println(e.getMessage());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-}
-class UnsupportedInputException extends Exception {
+    public static void main(String[] args) {
+        try (
+            Scanner input = new Scanner(System.in);
+        ) {
+            // Instantiate Variables
+            double apy = -1, apr = -1, balance, interest, accured = -1, sum = 0;
+            float comp = 1f;
+            String choice = "";
+            
+            do {
+                // Prompt user for APY
+                System.out.print("Enter your APY (Annual Percentage Yield): ");
+                apy = input.nextDouble();
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	
-	private double negativeWeight;
-	private String reason;
-	
-	public UnsupportedInputException(double negativeWeight, String reason) {
-		setNegativeWeight(negativeWeight);
-		setReason(reason);
-	}
+                // Create error/exception if APY input is negative value
+                if (apy < 0) {
+                    System.out.printf("%nError:\t%.2f is invalid. APY cannot be negative%n%n", apy);
+                }
+            } while (apy < 0);
+            
+            // Prompt user for compounding-frequency of APY
+            System.out.printf("%nDaily = 0\tWeekly = 1\tMonthly = 2%n"
+            		+ "How often does your Annual Percentage Yield Compound?: ");
 
-	public double getNegativeWeight() {
-		return negativeWeight;
-	}
+            // Fill Number of Periods value and store choice
+            switch (input.nextInt()) {
+                case 0:
+                    comp = 365f;
+                    accured = 365;
+                    choice = "Daily";
+                    break;
+                    
+                case 1:
+                    comp = 52f;
+                    accured = 7;
+                    choice = "Weekly";
+                    break;
+                    
+                case 2:
+                    comp = 12f;
+                    accured = 31;
+                    choice = "Monthly";
+                    break;
+            }
+            // Calculate and print APR
+            apr = (((Math.pow((apy / 100) + 1, (1 / comp))) - 1) * comp) * 100;
 
-	public void setNegativeWeight(double negativeWeight) {
-		this.negativeWeight = negativeWeight;
-	}
-	
-	public String getReason() {
-		return reason;
-	}
+            System.out.printf("%nAn APY of %.2f%c compounding %s is equivalant to: %.2f%c APR%n", apy, '%', choice, apr, '%');
 
-	public void setReason(String reason) {
-		this.reason = reason;
-	}
+            // Trim APR to two decimal positions
+            apr = Math.round(apr * 100.0);
+            apr = (apr / 100) / comp;
+            do {
+                // Prompt user for balance
+                System.out.printf("%nPlease enter your balance to calculate your estimated %s interest: ", choice);
+                balance = input.nextDouble();
 
-	public String getMessage() {
-		return String.format("ERROR: %.1f input-- %s Please restart the program and enter proper input.", getNegativeWeight(), getReason());
-	}
+                // Throw an error if a negative value is entered
+                if (balance < 0) {
+                    System.out.printf("%nError:\t%.2f is invalid. Balance cannot be negative%n%n", balance);
+                }
+            } while (balance < 0);
+
+            System.out.printf("%n%n");
+
+            for (int i = 1; i <= comp; i++) {
+                interest = (balance * apr) / 100;
+                balance += interest;
+                sum += interest;
+                System.out.printf("%s INTEREST #%d:" +
+                    "%n\t\t\t Daily Estimated Interest: $%,.4f" +
+                    "%n\t\t\t %s Earned Interest: $%,.4f" +
+                    "%n\t\t\t Updated Balance: $%,.2f %n",
+                    choice, i, interest / accured, choice, interest, balance);
+            }
+            System.out.printf("%n%n" +
+                "Starting Balance:\t\t$%,.2f %n" +
+                "Earning %s interest at: \t%.2f APY %n" +
+                "Total Interest Earned in Year:\t$%,.2f %n" +
+                "Ending Balance:\t\t\t$%,.2f",
+                balance - sum, choice, apy, sum, balance);
+            
+        } catch (InputMismatchException e) {
+            System.out.printf("%nEXCEPTION: %s%nRestart the program and be sure to input the correct type.", e.toString());
+       
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
